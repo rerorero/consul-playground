f40a67e30247357479a529b92b86f55742a80eda
rerorero <nato9598@hotmail.co.jp>
2019-10-30 09:16:59 +0900

    add grpc echo

--- cmd/echo/main.go
+++ cmd/echo/main.go
@@ -1,22 +1,31 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net"
 	"net/http"
+
+	"golang.org/x/sync/errgroup"
+
+	echo "github.com/rerorero/consul-playground/proto"
+
+	"google.golang.org/grpc"
 
 	"github.com/kelseyhightower/envconfig"
 )
 
 // Env is env
 type Env struct {
-	ID   string `envconfig:"ID"`
-	Port int    `envconfig:"PORT"`
+	ID       string `envconfig:"ID"`
+	HTTPPort int    `envconfig:"HTTP_PORT"`
+	GRPCPort int    `envconfig:"GRPC_PORT"`
 }
 
 func (s *Env) echo(w http.ResponseWriter, r *http.Request) {
-	log.Printf("[echo:%s] receive request\n", s.ID)
+	log.Printf("[echo:%s] receive HTTP request\n", s.ID)
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		log.Println(err)
@@ -29,10 +38,40 @@ func (s *Env) echo(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "%s from %s", string(body), s.ID)
 }
 
+func (s *Env) Echo(ctx context.Context, req *echo.EchoRequest) (*echo.EchoResponse, error) {
+	log.Printf("[echo:%s] receive GRPC request\n", s.ID)
+	return &echo.EchoResponse{
+		Message: fmt.Sprintf("%s from %s", req.Message, s.ID),
+	}, nil
+}
+
 func main() {
 	var env Env
 	envconfig.Process("", &env)
 
-	http.HandleFunc("/", env.echo) // hello
-	http.ListenAndServe(fmt.Sprintf(":%d", env.Port), nil)
+	log.Printf("[echo:%s] started", env.ID)
+
+	var wg errgroup.Group
+
+	// HTTP
+	wg.Go(func() error {
+		http.HandleFunc("/", env.echo) // hello
+		return http.ListenAndServe(fmt.Sprintf(":%d", env.HTTPPort), nil)
+	})
+
+	// GRPC
+	wg.Go(func() error {
+
+		lis, err := net.Listen("tcp", fmt.Sprintf(":%d", env.GRPCPort))
+		if err != nil {
+			log.Fatalf("[echo:%s] failed to listen: %v", env.ID, err)
+		}
+		s := grpc.NewServer()
+		echo.RegisterEchoServer(s, &env)
+		return s.Serve(lis)
+	})
+
+	if err := wg.Wait(); err != nil {
+		log.Printf("error occured: %v\n", err)
+	}
 }
--- go.mod
+++ go.mod
@@ -3,10 +3,12 @@ module github.com/rerorero/consul-playground
 go 1.13
 
 require (
+	github.com/golang/protobuf v1.3.2
 	github.com/kelseyhightower/envconfig v1.4.0
 	golang.org/x/net v0.0.0-20191028085509-fe3aa8a45271 // indirect
+	golang.org/x/sync v0.0.0-20190423024810-112230192c58
 	golang.org/x/sys v0.0.0-20191029155521-f43be2a4598c // indirect
 	golang.org/x/text v0.3.2 // indirect
 	google.golang.org/genproto v0.0.0-20191028173616-919d9bdd9fe6 // indirect
-	google.golang.org/grpc v1.24.0 // indirect
+	google.golang.org/grpc v1.24.0
 )
